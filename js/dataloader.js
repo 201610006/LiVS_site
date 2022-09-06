1ba2c316c4e27db8c26c4147ba55d56d82319cdf
201610006 <32411082+201610006@users.noreply.github.com>
2022-09-06 10:35:39 +0800

    Update dataloader.js

--- js/dataloader.js
+++ js/dataloader.js
@@ -3,7 +3,9 @@ function loadStudy() {
 $.getJSON("https://201610006.github.io/LiVS_site/dataset/dataset.json", function(data) {
                 console.log("==="+data)
 		data.livs.forEach(function(study) {
-			var studyRow = '<tr><td>' +
+			var imgnum = Math.floor(Math.random()*20+60)
+			var studyRow = "<tr><td><img width='90%' style='margin-top:0px;' src='img/labs/case_'"+imgnum+".png />" +
+			'</td><td>' +
 			study.name + '</td><td>' +
 			study.sex + '</td><td>' +
 			study.age + '</td><td>' +
