226607be28b27ad3dc8b082d1f30dcb21dee230b
201610006 <32411082+201610006@users.noreply.github.com>
2022-09-06 08:47:04 +0800

    Add files via upload

---
+++ js/dataloader.js
@@ -0,0 +1,24 @@
+
+
+function loadStudy() {
+	
+$.getJSON("dataset/dataset.json", function(data) {
+		data.livs.forEach(function(study) {
+			var studyRow = '<tr><td>' +
+			study.name + '</td><td>' +
+			study.sex + '</td><td>' +
+			study.age + '</td><td>' +
+			study.StudyDate + '</td><td>' +
+			study.SliceThickness + '</td><td>' +
+			study.size + '</td><td>' +
+			study.density + '</td><td>' +
+			study.space + '</td><td>' +
+			study.description + '</td><td>' +
+			"Download" + '</td>' +
+			'</tr>';
+
+		// Append the row to the study list
+		var studyRowElement = $(studyRow).appendTo('#studyListData');
+		})
+	})
+}
